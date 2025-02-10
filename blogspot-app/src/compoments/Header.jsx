dee4107913abf50fe4adfd70d901be6788ab8e99
Michael <michaelbrian88@gmail.com>
2025-02-10 21:30:04 +0100

    header and some styles

--- blogspot-app/src/compoments/Header.jsx
+++ blogspot-app/src/compoments/Header.jsx
@@ -1,15 +1,32 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../Images/logo.png";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-    <nav>
-        <div className="container nav__container">
-        <link to="/" className="nav__logo">
-        <img src="" alt=""/>
-        </link>
-    </div>
-    </nav>
-    )
-    }
-    export default Header
+        <nav>
+            <div className="container nav__container">
+                <Link to="/" className="nav__logo">
+                    <img src={Logo} alt="Navbar Logo"/>
+                </Link>
+                
+                <ul className={`nav__menu ${isOpen ? "open" : ""}`}>
+                    <li><Link to="/profile">Ernest Achiever</Link></li>
+                    <li><Link to="/create">Create Post</Link></li>
+                    <li><Link to="/authors">Authors</Link></li>
+                    <li><Link to="/logout">Logout</Link></li>
+                </ul>
+
+                <button className="nav__toggle-btn" onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen ? <AiOutlineClose/> : <FaBars />}
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
