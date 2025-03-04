d8d07b619518e97764dd692aa4459882a32b4572
Michael <michaelbrian88@gmail.com>
2025-03-04 22:51:45 +0100

    connecting app & api - edit post

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -2,15 +2,17 @@ import { useState, useContext, useEffect} from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"
 import { UserContext } from "../context/userContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const EditPost = () => {
     const [title, setTitle] = useState("");
     const [category, setCategory] = useState("uncategorized");
     const [description, setDescription] = useState("");
     const [thumbnail, setThumbnail] = useState("");
-
+    const [error, setError] = useState('')
     const navigate = useNavigate();
+    const {id} = useParams();
 
     const {currentUser} = useContext(UserContext)
     const token = currentUser?.token;
@@ -42,14 +44,51 @@ const EditPost = () => {
     const POST_CATEGORIES = ["Agriculture", "Business", "Education", "Entertainment", "Art", "Investment",
         "Uncategorized", "Weather"];
 
+
+        useEffect(() =>{
+          const getPost = async() => {
+            try{
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/posts/${id}`)
+                setTitle(response.data.title)
+                setDescription(response.data.description)
+            } catch (error) {
+                console.log(error)
+            }
+          }
+           getPost();
+        }, [])
+
+
+        const editPost = async (e) => {
+         e.preventDefault();
+         
+         const postData = new FormData();
+         postData.set('title', title)
+         postData.set('category', category)
+         postData.set('description', description)
+         postData.set('thumbnail', thumbnail)
+   
+       try {
+           const response = await axios.patch(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, postData, {withCredentials:true, 
+               headers: {Authorization: `Bearer ${token}`}})
+               if(response.status == 200){
+                   return navigate ('/')
+               }
+       } catch (error) {
+          setError (error.response.data.message);
+       }
+   
+       }
+        
+
     return (
         <section className="create-post">
             <div className="container">
                 <h2>Edit Post</h2>
-                <p className="form__error-message">
-                    This is an error message
-                </p>
-                <form className="form create-post__form">
+                {error && <p className="form__error-message">
+                    {error}
+                </p>}
+                <form className="form create-post__form" onSubmit={editPost}>
                     <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} autoFocus />
                     <select name="category" value={category} onChange={e => setCategory(e.target.value)}>
                         {POST_CATEGORIES.map(cat => (
