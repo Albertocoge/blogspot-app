1cf6d1fbe148c7fee3e3a44a17db219e8f5a645f
Michael <michaelbrian88@gmail.com>
2025-03-04 23:23:07 +0100

    connecting app & api - delete post

--- src/pages/DeletePost.jsx
+++ src/pages/DeletePost.jsx
@@ -1,22 +1,43 @@
 import { useContext, useEffect } from 'react';
-import {UserContext} from '../context/userContext.jsx'
-import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from '../context/userContext.jsx';
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import axios from 'axios';
 
-const DeletePost = () => {
+const DeletePost = ({ postId: id }) => {
     const navigate = useNavigate();
+    const location = useLocation();
 
-    const {currentUser} = useContext(UserContext)
+    const { currentUser } = useContext(UserContext);
     const token = currentUser?.token;
 
-    // redirect to login page for any user who isn´t logged in
+    
     useEffect(() => {
-        if(!token){
-            navigate('/login')
+        if (!token) {
+            navigate('/login');
         }
-    }, [])
-    return (
-        <Link className='btn sm danger'>Delete</Link>
-    )
+    }, [token, navigate]); 
 
-    }
-    export default DeletePost
+    const removePost = async () => {
+        try {
+            const response = await axios.delete(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, {
+                withCredentials: true, 
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            if (response.status === 200) {
+                if (location.pathname === `/myposts/${currentUser.id}`) {
+                    navigate(0); 
+                } else {
+                    navigate('/'); 
+                }
+            }
+        } catch (error) {
+            console.log("Couldn't delete post.");
+        }
+    };
+
+    return (
+        <Link className='btn sm danger' onClick={removePost}>Delete</Link> 
+    );
+};
+
+export default DeletePost;
